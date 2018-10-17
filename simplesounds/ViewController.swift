551d9877b68401f183e8f0e480a9b952afe4cc39
Eric Marschner <emarschn@gmail.com>
2018-10-16 21:48:04 -0600

    minor refactor

--- simplesounds/ViewController.swift
+++ simplesounds/ViewController.swift
@@ -160,17 +160,17 @@ class ViewController: UIViewController {
     func handleAnimations() {
         if let colorChanges = ViewController.colorChanges {
             for colorChange in colorChanges {
-            for but in buts {
-                if (but.tag == colorChange.tag) {
-                    but.layer.backgroundColor = colorChange.startColor
-                    UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveEaseInOut, animations: {
-                        but.layer.backgroundColor = colorChange.endColor
-                    }) { (finished) in
-                        ViewController.colorChanges = nil
+                for but in buts {
+                    if (but.tag == colorChange.tag) {
+                        but.layer.backgroundColor = colorChange.startColor
+                        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveEaseInOut, animations: {
+                            but.layer.backgroundColor = colorChange.endColor
+                        }) { (finished) in
+                            ViewController.colorChanges = nil
+                        }
+                        break;
                     }
-                    break;
                 }
-            }
             }
         }
     }
@@ -465,50 +465,5 @@ class ViewController: UIViewController {
         return UIColor.deepRedColor
     }
 
-}
-
-extension UIButton {
-    
-    private func degreesToRadians(_ x: CGFloat) -> CGFloat {
-        return .pi * x / 180.0
-    }
-    
-    func shake(duration: Double = 0.25, displacement: CGFloat = 1.0, degreesRotation: CGFloat = 2.0) {
-            let negativeDisplacement = -1.0 * displacement
-            let position = CAKeyframeAnimation.init(keyPath: "position")
-            position.beginTime = 0.8
-            position.duration = duration
-            position.values = [
-                NSValue(cgPoint: CGPoint(x: negativeDisplacement, y: negativeDisplacement)),
-                NSValue(cgPoint: CGPoint(x: 0, y: 0)),
-                NSValue(cgPoint: CGPoint(x: negativeDisplacement, y: 0)),
-                NSValue(cgPoint: CGPoint(x: 0, y: negativeDisplacement)),
-                NSValue(cgPoint: CGPoint(x: negativeDisplacement, y: negativeDisplacement))
-            ]
-            position.calculationMode = "linear"
-            position.isRemovedOnCompletion = false
-            position.repeatCount = Float.greatestFiniteMagnitude
-            position.beginTime = CFTimeInterval(Float(arc4random()).truncatingRemainder(dividingBy: Float(25)) / Float(100))
-            position.isAdditive = true
-            
-            let transform = CAKeyframeAnimation.init(keyPath: "transform")
-            transform.beginTime = 2.6
-            transform.duration = duration
-            transform.valueFunction = CAValueFunction(name: kCAValueFunctionRotateZ)
-            transform.values = [
-                degreesToRadians(-1.0 * degreesRotation),
-                degreesToRadians(degreesRotation),
-                degreesToRadians(-1.0 * degreesRotation)
-            ]
-            transform.calculationMode = "linear"
-            transform.isRemovedOnCompletion = false
-            transform.repeatCount = Float.greatestFiniteMagnitude
-            transform.isAdditive = true
-            transform.beginTime = CFTimeInterval(Float(arc4random()).truncatingRemainder(dividingBy: Float(25)) / Float(100))
-            
-            self.layer.add(position, forKey: "pos")
-            self.layer.add(transform, forKey: "trans")
-    }
-    
 }
 
